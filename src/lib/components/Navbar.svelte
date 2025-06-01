e95f3de99366687691f139ce5704c67ac7180589
ls00X <staerlau@students.zhaw.ch>
2025-06-01 06:56:44 +0200

    feat: restored Navbar.svelte

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,8 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark px-4">
+  <a class="navbar-brand" href="/">Vault of Legends</a>
+  <div class="navbar-nav">
+    <a class="nav-link" href="/characters">Charaktere</a>
+    <a class="nav-link" href="/classes">Klassen</a>
+    <a class="nav-link" href="/species">Spezies</a>
+  </div>
+</nav>
